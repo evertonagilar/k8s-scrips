ad4aeaafb98553b612d1c5aa7e4e49fd304c49f7
Everton de Vargas Agilar <evertonagilar@ufsm.br>
2023-10-10 16:03:41 -0300

    feat: kind-scripts

--- kind/cria-nginx-ingress-controller.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-
-kubectl wait --namespace ingress-nginx \
-  --for=condition=ready pod \
-  --selector=app.kubernetes.io/component=controller \
-  --timeout=120s
-
